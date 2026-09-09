d8509b21710df38a666fe7ac8df4c3518f53a6f8
Jermaine Mullins <wund3rbar9@gmail.com>
2026-09-09 17:12:29 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home"
+import Books from "./pages/Books"
+import { books } from "./data";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        
+        <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/books" render={() => <Books books={books}/>} />
+        
+        </Routes>
+        <Footer />
+       </div>
+    </Router>
+  );
+}
+
+export default App;
+
+
