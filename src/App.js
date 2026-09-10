489fa9dd8758adecfa0cfdaf5676097dea31cd6f
Jermaine Mullins <wund3rbar9@gmail.com>
2026-09-10 18:47:17 -0400

    making it dynamic

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import Books from "./pages/Books"
 import { books } from "./data";
-
+import BookInfo from "./pages/BookInfo";
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
         <Nav />
         
         <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/books" render={() => <Books books={books}/>} />
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books books={books}/>} />
+        <Route path="/books/:id" element={<BookInfo books={books}/>} />
         </Routes>
         <Footer />
        </div>
