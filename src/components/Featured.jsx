41f29c1caae9992047dcf5779352484f346a422b
Jermaine Mullins <wund3rbar9@gmail.com>
2026-09-17 15:02:57 -0400

    latest deployment

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -5,9 +5,7 @@ import { books } from '../data'
 const Featured = () => {
   console.log(books);
   console.log()
-  function getFiveStarBooks() {
-    
-  }
+ 
   return (
     <section id="features">
         <div className="container">
--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
+import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Rating from '../components/ui/Rating'
 import Price from '../components/ui/Price'
@@ -7,7 +7,7 @@ import Book from '../components/ui/Book'
 
 const BookInfo = ({ books, addToCart, cart }) => {
     const { id } = useParams();
-    const book = books.find((book) => +book.id == +id);
+    const book = books.find((book) => +book.id === +id);
 
     function addBookToCart(book) {
         addToCart(book);
